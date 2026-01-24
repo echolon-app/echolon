b55a37985970fbe6733bdb90e4f8dfe674e3aa81
Echolon <services@modrena.com>
2026-01-24 23:49:56 +0100

    chore(web): add build files for v1.1.0

---
+++ web/dist/echolon-web.es.js
@@ -0,0 +1,8 @@
+import { W as a, i as s, m as d, u as r } from "./index--UGIozHV.mjs";
+export {
+  a as WebModeProvider,
+  s as default,
+  d as mount,
+  r as useWebMode
+};
+//# sourceMappingURL=echolon-web.es.js.map
